ea59e37d062d48ee66f2a3d702daccfedd77e1f0
Gregory Mason <gregrockss@gmail.com>
2016-03-10 22:22:32 -0500

    Reset the classes
    Reset the classes to prepare for try #3

--- src/main/java/com/gmail/gregrockss/GregCraft/Main.java
+++ src/main/java/com/gmail/gregrockss/GregCraft/Main.java
@@ -4,11 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	public void onEnable(){
-		getServer().getPluginManager().registerEvents(new GregCraftListener(), this);
 		getLogger().info("onEnable has been invoked!");
 	}
 
-	@Override
 	public void onDisable() {
 		getLogger().info("onDisable has been invoked!");
 	}
