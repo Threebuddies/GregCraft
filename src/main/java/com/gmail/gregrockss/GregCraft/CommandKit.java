d2953678f8688c804285f213e9750aea4e97b532
Gregory Mason <gregrockss@gmail.com>
2016-03-11 00:45:55 -0500

    Tests if command was sent by player
    As title

--- src/main/java/com/gmail/gregrockss/GregCraft/CommandKit.java
+++ src/main/java/com/gmail/gregrockss/GregCraft/CommandKit.java
@@ -1,5 +1,6 @@
 package com.gmail.gregrockss.GregCraft;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,23 +9,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class CommandKit implements CommandExecutor {
-
-public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    if (sender instanceof Player) {
-        Player player = (Player) sender;
-        
-        // Create a new ItemStack and sets the number of items 
-        ItemStack diamond = new ItemStack(Material.DIAMOND, 3);
-        // Create a new ItemStack
-        ItemStack bricks = new ItemStack(Material.BRICK);
-        
-        // Set the amount of the ItemStack
-        bricks.setAmount(20);
-        
-        // Give the player our items (comma-seperated list of all ItemStack)
-        player.getInventory().addItem(bricks, diamond);
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    
+		if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can get kits!");
+            return true;
     }
-
-    return true;
+		
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+        
+			// Create a new ItemStack and sets the number of items 
+			ItemStack diamond = new ItemStack(Material.DIAMOND, 3);
+			// Create a new ItemStack
+			ItemStack bricks = new ItemStack(Material.BRICK);
+        
+			// Set the amount of the ItemStack
+    	   bricks.setAmount(20);
+        
+    	   // Give the player our items (comma-seperated list of all ItemStack)
+    	   player.getInventory().addItem(bricks, diamond);
+		}
+		return true;
 	}
 }
