20db01a257b3fe6302915c1e7925dd3ebf2761d4
Gregory Mason <gregrockss@gmail.com>
2016-03-16 00:34:53 -0400

    Re did bandages
    Right click on paper works on blocks other than air.

--- src/main/java/com/gmail/gregrockss/GregCraft/Healing.java
+++ src/main/java/com/gmail/gregrockss/GregCraft/Healing.java
@@ -4,7 +4,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -12,22 +11,17 @@ public class Healing implements Listener {
 	
 	@EventHandler
 	public void paperHealing(PlayerInteractEvent e) {
-		if (e.getAction() == Action.RIGHT_CLICK_AIR) {
-			if (e.getItem().getType() == Material.PAPER) {
-				Player p = e.getPlayer();
-				if (p.getHealth() < 14 ) {
-					final double pHealth = e.getPlayer().getHealth();
-					// This is for debugging final double fHealth = (pHealth + 6);
-					p.setHealth(pHealth + 6);
-					p.getInventory().removeItem(new ItemStack(Material.PAPER, 1));
-					// This is also for debugging System.out.println(("pHealth: ") + (pHealth) + (" fHealth") + (fHealth));
-				}else if (p.getHealth() > 14 && p.getHealth() < 20) {
-					final double health = e.getPlayer().getHealth();
-					final double pHealthF = 20 - e.getPlayer().getHealth();
-					// What do you know, also for debugging System.out.println(("health: ") + (health) + (" pHealthF: ") + (pHealthF) );
-					p.setHealth(health + pHealthF);
-					p.getInventory().removeItem(new ItemStack(Material.PAPER, 1));
-				}
+		if (e.getPlayer().getInventory().getItemInMainHand().getType() == Material.PAPER) {
+			Player p = e.getPlayer();
+			if (p.getHealth() < 14 ) {
+				final double pHealth = e.getPlayer().getHealth();
+				p.setHealth(pHealth + 6);
+				p.getInventory().removeItem(new ItemStack(Material.PAPER, 1));
+			}else if (p.getHealth() >= 14 && p.getHealth() < 20) {
+				final double health = e.getPlayer().getHealth();
+				final double pHealthF = 20 - e.getPlayer().getHealth();
+				p.setHealth(health + pHealthF);
+				p.getInventory().removeItem(new ItemStack(Material.PAPER, 1));
 			}
 		}
 	}
